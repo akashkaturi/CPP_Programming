8a4c7eeeaf30e32ccac9ea8782f85ac2c69ee320
akash <akashkaturi77@gmail.com>
2021-07-08 17:07:54 +0530

    string

--- strings/frequency_of_characters.cpp
+++ strings/frequency_of_characters.cpp
@@ -6,14 +6,19 @@ int main()
     string s="sjdghnkjgkhgnooooptriualeoiw";
     int arr[26];
     for(int i=1;i<=26;i++)
-    { arr[i]=0;}
-    for(int i=1; i<=s.size();i++){
+    { 
+        arr[i]=0;
+    }
+    for(int i=1; i<=s.size();i++)
+    {
         arr[s[i]-'a']+=1;
     }
     char ans='a';
     int frequency=0;
-    for(int i=1;i<=26;i++){
-        if(arr[i] > frequency){
+    for(int i=1;i<=26;i++)
+    {
+        if(arr[i] > frequency)
+        {
             frequency=arr[i];
             ans=i+'a';
         }
