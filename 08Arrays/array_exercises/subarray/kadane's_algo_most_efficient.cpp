e6e52193a9277dfc3ad4e348d7e0084cd90f34eb
akash <akashkaturi77@gmail.com>
2021-08-10 12:26:03 +0530

    Aug 10

--- 08Arrays/array_exercises/subarray/kadane's_algo_most_efficient.cpp
+++ 08Arrays/array_exercises/subarray/kadane's_algo_most_efficient.cpp
@@ -1,8 +1,28 @@
+// Max sum of Longest subbarray
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
 int main()
 {
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int currsum = 0;
+    int max_sum = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        currsum += arr[i];
+        if (currsum < 0)
+        {
+            currsum = 0;
+        }
+        max_sum = (currsum, currsum);
+    }
+    cout << max_sum << endl;
 
     return 0;
 }
