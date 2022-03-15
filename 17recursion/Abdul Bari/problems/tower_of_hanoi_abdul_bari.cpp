e0ac50b9c96bb8a55075b2010270b5232449f724
Akash Katuri <akashkaturi77@gmail.com>
2022-03-15 15:40:51 +0530

    hey

--- 17recursion/Abdul Bari/problems/tower_of_hanoi_abdul_bari.cpp
+++ 17recursion/Abdul Bari/problems/tower_of_hanoi_abdul_bari.cpp
@@ -9,13 +9,13 @@ void tower(int n, char a, char b, char c)
     {
         return;
     }
-    tower(n - 1, a, c, b);
+    tower(n - 1, a, c, b); //here source is a destination is c and helper is b
     cout << "Move from: " << a << " to " << c << endl;
-    tower(n - 1, b, c, a);
+    tower(n - 1, b, c, a); // here the source is b destnation is c and helper is a
 }
 int main()
 {
-    tower(3, 'a', 'b', 'c');
+    tower(5, 'a', 'b', 'c');
 
     return 0;
 }
